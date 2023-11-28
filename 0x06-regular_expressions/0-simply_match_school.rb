0bf2b5fde9eb98cd7cea59e49fa8ba3cf876269a
Kwesi20000 <hendrickskumi@yahoo.com>
2023-11-28 09:00:23 -0800

    hendy

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(argument)
-  regex = /School/
-  if argument.match?(regex)
-    puts "#{argument}"
-  else
-    puts "No match found for the term 'School' in the argument: #{argument}"
-  end
-end
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Please provide an argument."
-else
-  argument = ARGV[0]
-  match_school(argument)
-end
+puts ARGV[0].scan(/School/).join
