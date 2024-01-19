eeb8d36b3a67903fe195996307962108f59f92c2
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-19 12:32:10 -0800

    hendy

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Puppet manifest for installing Flask version 2.1.0
 
-package { 'python3-pip':
-  ensure => 'installed',
-}
-
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin',
-  creates => '/usr/local/lib/python3.8/dist-packages/Flask-2.1.0.dist-info',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
