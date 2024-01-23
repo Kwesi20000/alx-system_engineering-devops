cc9316d052a275cf0c1fe9b4459894b101c0e7bc
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-23 14:21:27 -0800

    hendy

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# This 
+
+package { 'nginx':
+    ensure  => installed,
+}
+
+file_line { 'add-redirect-rule':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'rewrite ^ https://github.com/Kwesi20000 permanent;',
+  after   => 'listen 80 default_server;',
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+    ensure  => running,
+    require => Package['nginx'],
+}
