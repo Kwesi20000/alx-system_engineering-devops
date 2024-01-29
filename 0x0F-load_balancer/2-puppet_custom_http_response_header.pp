a2bae7637954a2c22755890755bf2779e85a3104
Kwesi20000 <hendrickskumi@yahoo.com>
2024-01-29 15:55:55 -0800

    hendy

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,18 @@
-# Installs an Nginx server with a Custom Header 'X-Served-By'
+# This installs an Nginx server with a Custom Header 'X-Served-By'
 
-# Update the package list before installing Nginx
 exec { 'update':
     provider => shell,
     command  => 'sudo apt-get -y update',
     before   => Exec['install Nginx'],
 }
 
-# Install Nginx
 exec { 'install Nginx':
     provider => shell,
     command  => 'sudo apt-get -y install nginx',
     before   => Exec['restart Nginx'],
 }
 
-# Restart Nginx after installation
 exec { 'restart Nginx':
-    provider => shell'
+    provider => shell,
     command  => 'sudo service nginx restart'
 }
